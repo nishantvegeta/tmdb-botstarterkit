56b70b44c9f51ad86bec69596c9bc15df98c88e2
Bala M <bala@Balas-MacBook-Pro.local>
2023-02-25 22:14:51 +0545

    Fixed in qrbot to get run_item through kwargs

--- qrlib/QRBot.py
+++ qrlib/QRBot.py
@@ -13,7 +13,6 @@ class QRBot(ABC, QRPublisher):
     def __init__(self):
         for base_class in QRBot.__bases__:
              base_class.__init__(self)
-        self.run_item: QRRunItem = None
 
     @abstractmethod
     def start(self):
@@ -25,13 +24,14 @@ class QRBot(ABC, QRPublisher):
 
     @run_item(is_ticket=False, post_error=False)
     def setup_platform_components(self, *args, **kwargs):
+        run_item: QRRunItem = kwargs['run_item']
         for vault_name in QREnv.VAULT_NAMES:
             try:
                 QREnv.VAULTS[vault_name] = qrlib.QRUtils.get_secret(vault_name)
-                self.run_item.logger.info(f"Retrieved vault: {vault_name}")
+                run_item.logger.info(f"Retrieved vault: {vault_name}")
             except Exception as e:
-                self.run_item.logger.error(f"Failed to retrieve vault: {vault_name}")
-                self.run_item.notification.set(subject=f"Warning! {QREnv.BOT_NAME} bot has stopped unexpectedly", data={"reason":f"Failed to retrieve vault: {vault_name}"})
+                run_item.logger.error(f"Failed to retrieve vault: {vault_name}")
+                run_item.notification.set(subject=f"Warning! {QREnv.BOT_NAME} bot has stopped unexpectedly", data={"reason":f"Failed to retrieve vault: {vault_name}"})
                 raise e
 
         for queue_name in QREnv.QUEUE_NAMES:
@@ -39,10 +39,10 @@ class QRBot(ABC, QRPublisher):
                 queue = QRQueue(queue_name)
                 queue.get_info()
                 QREnv.QUEUES[queue_name] = queue
-                self.run_item.logger.info(f"Retrieved queue info: {queue_name}")
+                run_item.logger.info(f"Retrieved queue info: {queue_name}")
             except Exception as e:
-                self.run_item.logger.error(f"Failed to retrieve queue info: {queue_name}")
-                self.run_item.notification.set(subject=f"Warning! {QREnv.BOT_NAME} bot has stopped unexpectedly", data={"reason":f"Failed to retrieve queue info: {queue_name}"})
+                run_item.logger.error(f"Failed to retrieve queue info: {queue_name}")
+                run_item.notification.set(subject=f"Warning! {QREnv.BOT_NAME} bot has stopped unexpectedly", data={"reason":f"Failed to retrieve queue info: {queue_name}"})
                 raise e
 
         for storage_name in QREnv.STORAGE_NAMES:
@@ -50,9 +50,9 @@ class QRBot(ABC, QRPublisher):
                 storage = QRStorageBucket(storage_name)
                 storage.get_info()
                 QREnv.STORAGES[storage_name] = storage
-                self.run_item.logger.info(f"Retrieved storage info: {storage_name}")
+                run_item.logger.info(f"Retrieved storage info: {storage_name}")
             except Exception as e:
-                self.run_item.logger.error(f"Failed to retrieve storage info: {storage_name}")
-                self.run_item.notification.set(subject=f"Warning! {QREnv.BOT_NAME} bot has stopped unexpectedly", data={"reason":f"Failed to retrieve storage info: {storage_name}"})
+                run_item.logger.error(f"Failed to retrieve storage info: {storage_name}")
+                run_item.notification.set(subject=f"Warning! {QREnv.BOT_NAME} bot has stopped unexpectedly", data={"reason":f"Failed to retrieve storage info: {storage_name}"})
                 raise e
 
